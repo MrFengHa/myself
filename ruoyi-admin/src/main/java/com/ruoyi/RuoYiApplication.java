4c6e96e829798dc3c14cf2a53b12c338518edfdd
MrFengHa <110179567@qq.com>
2021-06-29 22:42:52 +0800

    增加docker上传 以及显示端口号

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,9 +1,18 @@
 package com.ruoyi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
 /**
  * 启动程序
@@ -12,8 +21,11 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan(basePackages = {"com.ruoyi.*"})
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
+public class RuoYiApplication  implements CommandLineRunner
 {
+    @Autowired
+    private Environment environment;
+    private static final Logger logger = LoggerFactory.getLogger(RuoYiApplication.class);
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
@@ -28,5 +40,25 @@ public class RuoYiApplication
                 " |  | \\ `'   /|   `-'  /           \n" +
                 " |  |  \\    /  \\      /           \n" +
                 " ''-'   `'-'    `-..-'              ");
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        try {
+            logger.info(
+                    "\n------------------------环境信息---------------------------\n\t"
+                            + "Application '{}' is running! Access URLs:\n\t"
+                            + "Local  : \thttp://{}:{}\n\t"
+                            + "Swagger: \thttp://{}:{}/api/swagger-ui.html\n\t"
+                            + "Profile(s): \t{}\n----------------------------------------------------------",
+                    environment.getProperty("spring.application.name"),
+                    InetAddress.getLocalHost().getHostAddress(),
+                    environment.getProperty("server.port"),
+                    InetAddress.getLocalHost().getHostAddress(),
+                    environment.getProperty("server.port"),
+                    Arrays.toString(environment.getActiveProfiles()));
+        } catch (UnknownHostException e) {
+            logger.error("spring boot...启动时发生异常", e);
+        }
     }
 }
